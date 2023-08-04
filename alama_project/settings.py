4dab28482bccfd65f7b3dedf48b065088b01194e
mudabek <nazarov.otabek66@gmail.com>
2023-08-04 15:29:17 +0500

    quick

--- alama_project/settings.py
+++ alama_project/settings.py
@@ -142,7 +142,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
